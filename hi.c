b0134b220b0cc707cb13fd385c6c086a6fb71ead
Sunny Kumar <sunny.kumar.roy@gmail.com>
2016-10-06 11:28:27 +0530

    Add files via upload

---
+++ hi.c
@@ -0,0 +1,32 @@
+/** @file
+  Brief Description of UEFI MyHelloWorld
+  Detailed Description of UEFI MyHelloWorld
+  Copyright for UEFI MyHelloWorld
+  License for UEFI MyHelloWorld
+**/
+
+#include <Uefi.h>
+#include <Library/UefiApplicationEntryPoint.h>
+#include <Library/UefiLib.h>
+
+/**
+  as the real entry point for the application.
+
+  @param[in] ImageHandle    The firmware allocated handle for the EFI image.  
+  @param[in] SystemTable    A pointer to the EFI System Table.
+  
+  @retval EFI_SUCCESS       The entry point is executed successfully.
+  @retval other             Some error occurs when executing this entry point.
+
+**/
+EFI_STATUS
+EFIAPI
+UefiMain (
+  IN EFI_HANDLE        ImageHandle,
+  IN EFI_SYSTEM_TABLE  *SystemTable
+  )
+{
+  Print(L"Sunny's App \n"); 
+  return EFI_SUCCESS;
+}
+
